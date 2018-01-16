205fd9aba8cd37612a3b4f28bc50133b62381bd1
Sergey Kuptsov <kuptservol@yandex-team.ru>
2018-01-16 23:20:47 +0300

    catsvsdogs

---
+++ catsvsdogs/model.py
@@ -0,0 +1,88 @@
+from numpy.random import random, permutation
+from scipy import misc, ndimage
+from scipy.ndimage.interpolation import zoom
+import numpy as np
+
+import keras
+from keras import backend as K
+from keras.utils.data_utils import get_file
+from keras.models import Sequential, Model
+from keras.layers.core import Flatten, Dense, Dropout, Lambda
+from keras.layers import Input
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD, RMSprop
+from keras.preprocessing import image
+from matplotlib import pyplot as plt
+K.set_image_dim_ordering('th')
+from keras.optimizers import SGD, RMSprop, Adam
+                                        
+def ConvBlock(layers, model, filters):
+    for i in range(layers): 
+        model.add(ZeroPadding2D((1,1)))
+        model.add(Convolution2D(filters, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+def FCBlock(model):
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+
+# Mean of each channel as provided by VGG researchers
+vgg_mean = np.array([123.68, 116.779, 103.939], dtype=np.float32).reshape((3,1,1))
+
+def vgg_preprocess(x):
+    x = x - vgg_mean     # subtract mean
+    return x[:, ::-1]    # reverse axis bgr->rgb
+
+def get_batches(dirname, batch_size, gen=image.ImageDataGenerator(), shuffle=True, 
+                class_mode='categorical'):
+    return gen.flow_from_directory(dirname, target_size=(224,224), 
+                class_mode=class_mode, shuffle=shuffle, batch_size=batch_size)
+
+def VGG16():
+    model = Sequential()
+    model.add(Lambda(vgg_preprocess, input_shape=(3,224,224), output_shape=(3,224,224)))
+
+    ConvBlock(2, model, 64)
+    ConvBlock(2, model, 128)
+    ConvBlock(3, model, 256)
+    ConvBlock(3, model, 512)
+    ConvBlock(3, model, 512)
+
+    model.add(Flatten())
+    FCBlock(model)
+    FCBlock(model)
+    model.add(Dense(1000, activation='softmax'))
+    fpath = get_file('vgg16.h5', 'data/vgg16.h5', cache_subdir='models')
+    model.load_weights(fpath)
+
+    return model
+
+def finetune(model, batches):
+    ft(model, batches.nb_class)
+    classes = list(iter(batches.class_indices))
+        
+    for c in batches.class_indices:
+        classes[batches.class_indices[c]] = c
+    return classes
+
+def ft(model, num):
+    model.pop()
+    for layer in model.layers: layer.trainable=False
+    model.add(Dense(num, activation='softmax'))
+    model.compile(optimizer=Adam(lr=0.001),
+                loss='categorical_crossentropy', metrics=['accuracy'])
+
+def plots(ims, figsize=(12,6), rows=1, interp=False, titles=None):
+    if type(ims[0]) is np.ndarray:
+        ims = np.array(ims).astype(np.uint8)
+        if (ims.shape[-1] != 3):
+            ims = ims.transpose((0,2,3,1))
+    f = plt.figure(figsize=figsize)
+    cols = len(ims)//rows if len(ims) % 2 == 0 else len(ims)//rows + 1
+    for i in range(len(ims)):
+        sp = f.add_subplot(rows, cols, i+1)
+        sp.axis('Off')
+        if titles is not None:
+            sp.set_title(titles[i], fontsize=16)
+        plt.imshow(ims[i], interpolation=None if interp else 'none')
+
